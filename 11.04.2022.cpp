bf1a9e18b869738794bd6043405b871b92f6c5ba
Infendum <69817375+Infendum@users.noreply.github.com>
2022-04-11 21:03:39 +0600

    Update 11.04.2022.cpp

--- 11.04.2022.cpp
+++ 11.04.2022.cpp
@@ -40,3 +40,34 @@ int main()
 
     return 0;
 }
+
+
+
+
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+
+    /*strcpy_s(des, 256, sou)*/
+
+    const char* cstr = "alsdfhaljksf alsdfkhaklsdf";
+
+    char slova[256]{ 0 };
+    int podchet{ strlen(cstr) };
+    size_t schet{ 0 };
+    const char* pre_position{ cstr };
+    const char* position{ strchr(cstr, ' ') };
+    while (position != nullptr) {
+        
+        strncpy_s(slova, pre_position, position - pre_position);
+        pre_position = position;
+        schet++;
+        position = strchr(++position, ' ');
+    }
+    std::cout << schet + 1 << std::endl;
+
+    return 0;
+}

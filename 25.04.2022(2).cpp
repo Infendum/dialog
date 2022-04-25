98a286097174e567708f81f01d7b5f1cf3233b6e
Infendum <69817375+Infendum@users.noreply.github.com>
2022-04-25 21:52:04 +0600

    Create 25.04.2022(2).cpp

---
+++ 25.04.2022(2).cpp
@@ -0,0 +1,173 @@
+fraction.cpp
+
+
+
+#include "fraction.h"
+
+fraction::fraction()
+{
+	this->numerator = 0;
+	this->denominator = 0;
+}
+
+fraction::fraction(const unsigned int num, const unsigned int den)
+{
+	this->numerator = num;
+	this->denominator = den;
+}
+
+fraction fraction::operator+(const fraction& obj)
+{
+	if (this->denominator == obj.denominator) {
+		return fraction(this->numerator + obj.numerator, denominator);
+	}
+	else if (this->denominator != obj.denominator) {
+		int numerator1 = obj.numerator;
+		int denomirator1 = obj.denominator;
+		this->numerator = this->numerator * denomirator1;
+		numerator1 = this->denominator * numerator1;
+		denominator = this->denominator * denomirator1;
+		denomirator1 == this->denominator * denomirator1;
+		return fraction(this->numerator + numerator1, denominator);
+	}
+	return fraction();
+}
+
+fraction fraction::operator-(const fraction& obj)
+{
+	if (this->denominator == obj.denominator) {
+		return fraction(this->numerator - obj.numerator, denominator);
+	}
+	else if (this->denominator != obj.denominator) {
+		int numerator1 = obj.numerator;
+		int denomirator1 = obj.denominator;
+		this->numerator = this->numerator * denomirator1;
+		numerator1 = this->denominator * numerator1;
+		denominator = this->denominator * denomirator1;
+		denomirator1 == this->denominator * denomirator1;
+		return fraction(this->numerator - numerator1, denominator);
+	}
+}
+
+fraction fraction::operator*(const fraction& obj)
+{
+	return fraction(this->numerator * obj.numerator, this->denominator * obj.denominator);
+}
+
+fraction fraction::operator/(const fraction& obj)
+{
+	return fraction(this->numerator * obj.denominator, this->denominator * obj.numerator);
+}
+
+fraction fraction::operator+(const int& value)
+{
+	fraction obj(value, 1) 
+		int numerator1 = obj.numerator;
+		int denomirator1 = obj.denominator;
+		this->numerator = this->numerator * 1;
+		value = this->denominator * value;
+		denominator = this->denominator * 1;
+		1 == this->denominator * 1;
+		return fraction(this->numerator + value, denominator);
+	
+	return fraction();
+}
+
+fraction fraction::operator-(const int& value)
+{
+	fraction obj(value, 1)
+		int numerator1 = obj.numerator;
+	int denomirator1 = obj.denominator;
+	this->numerator = this->numerator * 1;
+	value = this->denominator * value;
+	denominator = this->denominator * 1;
+	1 == this->denominator * 1;
+	return fraction(this->numerator + value, denominator);
+
+	return fraction();
+	return fraction();
+}
+
+fraction::operator float()
+{
+	if (this->numerator == 0 && this->denominator == 0)
+		return 0;
+	if (this->denominator == 0) {
+		std::cout << "error" << std::endl;
+		return FLT_MAX;
+	}
+	return (float)numerator / (float)denominator;
+}
+
+void fraction::show()
+{
+	std::cout << this->numerator;
+	std::cout << "/";
+	std::cout << this->denominator << std::endl;
+}
+
+
+
+
+
+
+fraction.h
+
+
+
+
+#pragma once
+#include <iostream>
+class fraction
+{
+	private:
+	unsigned int numerator;
+	unsigned int denominator;
+public:
+	fraction();
+	fraction(const unsigned int num, const unsigned int den);
+	fraction operator+(const fraction& obj);
+	fraction operator-(const fraction& obj);
+	fraction operator*(const fraction& obj);
+	fraction operator/(const fraction& obj);
+	fraction operator+(const int& value);
+	fraction operator-(const int& value);
+	operator float();
+	void show();
+};
+
+
+
+
+
+
+main
+
+
+
+
+
+#include <iostream>
+#include "fraction.h"
+
+int main()
+{
+	fraction first(3, 5), second(1, 2), third;
+
+	third = first + second;
+	third.show();
+	std::cout << third << std::endl << std::endl;
+
+	third = first - second;
+	third.show();
+	std::cout << third << std::endl << std::endl;
+
+	third = first * second;
+	third.show();
+	std::cout << third << std::endl << std::endl;
+
+	third = first / second;
+	third.show();
+	std::cout << third << std::endl << std::endl;
+	return 0;
+}

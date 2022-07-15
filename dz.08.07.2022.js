967d2212a713571cf4732cc25542e4d49bc8ac3e
Infendum <69817375+Infendum@users.noreply.github.com>
2022-07-15 17:28:57 +0600

    Create dz.08.07.2022.js

---
+++ dz.08.07.2022.js
@@ -0,0 +1,53 @@
+//задание номер 1
+//const sum = prompt('Введите имя');
+//alert('Привет' + sum);
+
+//задание номер 2
+//const sum = prompt('Введите дату рождения');
+//const summ = 2022 - sum;
+//alert('Дата вашего рождения' + summ);
+
+//задание номер 3
+//const sum = prompt('Введите сторону квадрата');
+//const summ = 4 * sum;
+//alert('Периметр квадрата' + summ);
+
+//задание номер 4
+//const sum = prompt('Введите радиус окружности');
+//const summ = 3.14 * (sum * sum);
+//alert('Площадь окружности' + summ);
+
+//задание номер 5
+//const km = prompt('Введите расстояние');
+//const hours = prompt('Введите часы');
+//const min = prompt('Введите минуты')
+//const summ = km / (hours + min / 60.0);
+//alert('Вашt время' + summ);
+
+//задание номер 6
+//const sum = prompt('Введите сумму');
+//const summ = sum / 1.01;
+//alert('Ваша сумма в евро' + summ);
+
+//задание номер 7
+//const sum = prompt('Введите гб');
+//const summ = (sum * 1000) / 820;
+//alert('Количество файлов' + summ);
+
+//задание номер 8
+//const sum = prompt('Введите сумму в кошельке');
+//const sum1 = prompt('Введите стоимость шоколадки');
+//const summ = sum / sum1;
+//const summ1 = sum - sum1;
+//alert('Количество шоколадок которое вы можете купить' + summ);
+//alert('Сдача' + summ1);
+
+//задание номер 9
+//sum = prompt('Введите трехзначное число');
+//result = parseInt(sum / 100);
+//result1 = sum / 10;
+//result1 = parseInt(result1 % 10);
+//result2 = sum % 10;
+//alert('Число' + result2 +  result1 +  result);
+
+//задание номер 10
